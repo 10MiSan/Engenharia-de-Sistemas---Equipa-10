6fdc7a5522cfe49abfec2b28cccbcaad831c12b0
MiSan <misan@example.com>
2026-05-12 11:37:49 +0100

    Funcionalidades Iniciais discutidas - falta implementar modelos de AI

--- esist-semantic-receiver/web_com_mqtt_mosquito/app.py
+++ esist-semantic-receiver/web_com_mqtt_mosquito/app.py
@@ -16,5 +16,5 @@ def receiver():
     return render_template("receiver.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 9000))
     app.run(host="0.0.0.0", port=port, debug=True)
